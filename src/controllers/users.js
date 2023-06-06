4e7adf2595ea387dafe4f1ea87cdf800649ac1fc
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-06 10:19:08 +0700

    Create controllers and routes for CRUD: create users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,10 +1,18 @@
 import Users from '../models/users.js';
+import messages from '../utils/messages.js';
 
 // CRUD to MySQL Database
 // CREATE - POST
-const createUser = (req, res) => {
-    const body = req.body;
-    res.send(body);
+const createUser = async (req, res) => {
+    const data = req.body;
+
+    try {
+        await Users.sync();
+        const result = await Users.create(data);
+        messages(res, 201, "Create user success", result);
+    } catch (error) {
+        messages(res, 500, "Internal server error");
+    }
 };
 
 export { createUser };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+// import cors from 'cors';
 import r_users from './routers/users.js';
 
 const app = express();
 
 const PORT = 9000;
 
+// app.use(cors());
 app.use(express.urlencoded({ limit:"5mb", extended: true }));
 app.use(express.json({ limit:"5mb" }));
 
