9a601324c24024b50487d74bc07e4648197daee7
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-07 17:37:51 +0700

    konfigurasi cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import r_users from './routers/users.js';
 
 const app = express();
 
 const PORT = 9000;
 
-// app.use(cors());
+//use cors
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ limit:"5mb", extended: true }));
+
+// parse application/json
 app.use(express.json({ limit:"5mb" }));
 
-// routers
+// use route in express
 app.use('/api/v1', r_users);
 
 app.listen(PORT, () => {
